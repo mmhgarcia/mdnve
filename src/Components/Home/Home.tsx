1f3b585b3c558de16f59c5cfbafd2dde2854ab43
Alberto G <mmhgarcia@users.noreply.github.com>
2021-10-29 12:26:24 -0400

    Home con ref no usadas. Header y Footer

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,8 @@
 import './Home.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cards from '../../Containers/Cards/Cards';
-import Header from '../Header/Header';
 import Author from '../Author/Author';
 import Signup from '../Signup/Signup';
-import Footer from '../Footer/Footer';
 
 function Home() {
   return (   
