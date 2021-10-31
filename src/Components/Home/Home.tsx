ad5bbe367a9969fc494a8e1f229fd326c868d413
Alberto G <mmhgarcia@users.noreply.github.com>
2021-10-31 15:18:16 -0400

    Modificacion home component.

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,8 +1,8 @@
 import './Home.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cards from '../../Containers/Cards/Cards';
-import Author from '../Author/Author';
-import Signup from '../Signup/Signup';
+//import Author from '../Author/Author';
+//import Signup from '../Signup/Signup';
 
 function Home() {
   return (   
